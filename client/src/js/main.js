701325811638600ad5acf4e7c910f283e60fba3a
Frank Filius <f.filius@amsterdam.nl>
2021-02-18 09:46:04 +0100

    Make fe-flow work

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -1,33 +1,53 @@
-import irma from "@privacybydesign/irma-frontend";
+import irma from '@privacybydesign/irma-frontend';
 
-const btnEl = document.querySelector("button");
-console.log(btnEl);
-btnEl.addEventListener("click", signVote);
+const btnEl = document.querySelector('button');
+btnEl.addEventListener('click', signVote);
 
-function signVote() {
-  console.log("signing");
-  irmaWeb
-    .start()
-    .then((result) => console.log("Successful disclosure! 🎉", result))
-    .catch((error) => console.error("Couldn't do what you asked 😢", error));
-}
+const signVote = async () => {
+    try {
+        const result = await irmaWeb.start();
+        const reducedResult = reduceIRMAResult(result.disclosed);
+        return reducedResult;
+    } catch (e) {
+        return null;
+    }
+};
+
+const reduceIRMAResult = disclosedCredentialSets => {
+    let joinedResults = {};
+    disclosedCredentialSets.forEach(conjunction => {
+        joinedResults = {
+            ...joinedResults,
+            ...conjunction.reduce(
+                (acc, { id, rawvalue }) => ({
+                    ...acc,
+                    [id.match(/[^.]*$/g)[0]]: rawvalue
+                }),
+                {}
+            )
+        };
+    });
+    return joinedResults;
+};
 
 const irmaWeb = irma.newWeb({
-  debugging: false, // Enable to get helpful output in the browser console
-  element: "#irma-web-form", // Which DOM element to render to
+    debugging: true,
+    element: '#irma-web-form',
+    session: {
+        url: 'http://localhost:8000',
 
-  // Back-end options
-  session: {
-    // Point this to your controller:
-    url: "http://localhost:8000",
-
-    start: {
-      url: (o) => `${o.url}/start`,
-      method: "GET",
-    },
-    result: {
-      url: (o, { sessionPtr, sessionToken }) => `${o.url}/result`,
-      method: "GET",
-    },
-  },
+        start: {
+            url: o => `${o.url}/start`,
+            method: 'GET'
+        },
+        mapping: {
+            sessionPtr: r => ({
+                ...r.sessionPtr,
+                u: r.sessionPtr.u.replace(/\/irma/g, '/irma/irma')
+            })
+        },
+        result: {
+            url: (o, { sessionToken }) => `${o.url}/result?token=${sessionToken}`
+        }
+    }
 });
